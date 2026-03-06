22ab231ee9979c1f71703d9a69db130e6c60544e
Lafabrication <toaviniainajuliena@gmail.com>
2026-03-06 09:06:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
-function acheter(){
-alert("Commande envoyée");
+let user=""
+
+function login(){
+
+user=document.getElementById("username").value
+
+if(user!=""){
+
+document.getElementById("loginBox").style.display="none"
+document.getElementById("marketBox").style.display="block"
+
+}
+
+}
+
+function publier(){
+
+let produit=document.getElementById("produit").value
+let prix=document.getElementById("prix").value
+let image=document.getElementById("image").value
+let description=document.getElementById("description").value
+
+let div=document.createElement("div")
+
+div.className="produit"
+
+div.innerHTML=
+"<img src='"+image+"'>"+
+"<h3>"+produit+"</h3>"+
+"<p>"+prix+" Ar</p>"+
+"<p>"+description+"</p>"+
+"<div class='actions'>"+
+"<button onclick='acheter(\""+produit+"\")'>Acheter</button>"+
+"<button onclick='supprimer(this)'>Supprimer</button>"+
+"</div>"
+
+document.getElementById("liste").appendChild(div)
+
+}
+
+function acheter(p){
+
+alert("Commande envoyée pour : "+p)
+
+}
+
+function supprimer(btn){
+
+btn.parentElement.parentElement.remove()
+
 }
